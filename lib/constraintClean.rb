80e345c3aa1e14781849c88b21b3f87ad5526f7b
Oleg Kokhtenko <olegkokhtenko@Olegs-MacBook-Pro.local>
2015-07-13 13:00:23 +0300

    never remove constraint related to IBOutlets + never remove constraints excluded from any variations except "default"

--- lib/constraintClean.rb
+++ lib/constraintClean.rb
@@ -6,19 +6,47 @@ def cleanupConstraints(file)
 	f = File.open(file)
 	doc = Nokogiri::XML(f)
 	f.close
-
-	excluded = doc.xpath('//exclude')
-	included = doc.xpath('//include')
+	
+	excluded = []
+	variations = doc.xpath('//variation')
+	
+	included   = doc.xpath('//include')
+	outlets    = doc.xpath('//outlet')
+	
+	variations.each do |variation|
+		if variation.attr('key') == "default"
+			ch = variation.children
+			ch.each do |mask|
+				constraints = mask.children
+				constraints.each do |constraint|
+					if constraint.name == 'exclude'
+						excluded.push(constraint)
+					end
+				end 
+			end
+		end
+	end
 	
 	result = [] 
 	excluded.each do |node| 
 		found = false
 		for includedNode in included
+		p node
 			if node.attr('reference') == includedNode.attr('reference')
 				found = true
 				break
 			end
 		end
+		
+		if !found 
+			for outlet in outlets
+				if node.attr('reference') == outlet.attr('destination')
+					found = true
+					break
+				end	
+			end
+		end 
+		
 		if !found 
 			result.push(node)
 			nodeID = node.attr('reference')
